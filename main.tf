6c34bf057f93a2c953cee8afcb900af5aefb698e
iamdempa <banukajananathjayarathna@gmail.com>
2020-10-08 14:11:35 +0530

    test

--- main.tf
+++ main.tf
@@ -18,7 +18,6 @@ terraform {
 module "network" {
   source = "./modules/network"
 
-  cluster_name           = "webservers-stage"
-  db_remote_state_bucket = "(YOUR_BUCKET_NAME)"
-  db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
+  name           = "network-boilerplate"
+  auto_create_subnetworks = "false"
 }
